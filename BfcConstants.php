59c69dd2a07f70e64a78ae1cb98c324c3f965fbf
Gabriel Comte <gabriel.comte@gmail.com>
2019-11-11 18:08:36 +0100

    Add settings page to WP backend

--- BfcConstants.php
+++ BfcConstants.php
@@ -4,8 +4,19 @@ class BfcConstants {
     // Basically the plugin's database.
     // All data is being serialized and stored in the table 'wp_options'.
     // These constants store the tags that are being used in the field wp_options.option_name .
-    const DB_STORAGE_KEY_SETTINGS = 'bitcoin-fortune-cookie';
     const DB_STORAGE_KEY_INVOICES = 'bitcoin-fortune-cookie-invoices';
+    const DB_STORAGE_KEY_BTCPAY_APP_LINK = 'bitcoin-fortune-cookie-btcpay-app-link';
 
     const FORTUNES_FILE_NAME = 'fortunes.txt';
+
+    // Settings page
+    const SETTINGS_PAGE_TITLE = 'Bitcoin Fortune Cookie';
+    const SETTINGS_PAGE_BROWSER_TITLE = 'Fortune Cookie Settings';
+    const SETTINGS_PAGE_SLUG = 'bitcoin-fortune-cookie-settings'; // What's added to the URL to create the settings pages' own unique URL
+    const SETTINGS_PAGE_DASHICON_SYMBOL = 'dashicons-star-half';
+    const SETTINGS_PAGE_MENU_TITLE = 'Fortune Cookie';
+    const SETTINGS_PAGE_POSITION = 66; // Just below the "Plugins" menu entry
+    const SETTINGS_PAGE_BTCPAY_SECTION_TITLE = 'BTCPay Settings';
+    const SETTINGS_PAGE_BTCPAY_SECTION_DESCRIPTION = 'Connection to BTCPay Server';
+    const SETTINGS_PAGE_BTCPAY_APP_LINK_FIELD_DESCRIPTION = 'BTCPay app URL';
 }
---
+++ wp-backend/SettingsPage.php
@@ -0,0 +1,85 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+require_once(dirname(__FILE__, 2) . '/BfcConstants.php');
+
+class SettingsPage {
+
+    const BTCPAY_SECTION_KEY = 'btcpay_section';
+    const BACKEND_CSS_LABEL = 'bitcoin-fortune-cookie-backend';
+    const BACKEND_CSS_FILE = 'wp-content/plugins/btcfortunecookie/wp-backend/bitcoin-fortune-cookie-backend.css';
+
+    public function __construct(){
+        // Allow page to call options.php and write settings to options table
+        register_setting(BfcConstants::SETTINGS_PAGE_SLUG, BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK);
+
+        add_action('admin_init', array($this, 'callback_for_setting_up_scripts'));
+    }
+
+    public function callback_for_setting_up_scripts() {
+        wp_register_style(self::BACKEND_CSS_LABEL, get_site_url(null, '/' . self::BACKEND_CSS_FILE));
+        wp_enqueue_style(self::BACKEND_CSS_LABEL);
+    }
+
+    public function injectPluginSettingsPage() {
+        // Hook into the admin menu
+        add_action('admin_menu', array($this, 'createPluginSettingsPage'));
+        add_action('admin_init', array( $this, 'setupSections'));
+        add_action('admin_init', array( $this, 'setupFields'));
+    }
+
+    public function createPluginSettingsPage() {
+
+        // Add the menu item and page
+        $capability = 'manage_options';
+        $callback = array( $this, 'pluginSettingsPageContent' );
+
+        add_menu_page(
+            BfcConstants::SETTINGS_PAGE_BROWSER_TITLE,
+            BfcConstants::SETTINGS_PAGE_MENU_TITLE,
+            $capability,
+            BfcConstants::SETTINGS_PAGE_SLUG,
+            $callback,
+            BfcConstants::SETTINGS_PAGE_DASHICON_SYMBOL,
+            BfcConstants::SETTINGS_PAGE_POSITION
+        );
+    }
+
+    public function setupSections(){
+        add_settings_section(self::BTCPAY_SECTION_KEY, BfcConstants::SETTINGS_PAGE_BTCPAY_SECTION_TITLE, array($this, 'btcpaySectionCallback'), BfcConstants::SETTINGS_PAGE_SLUG);
+    }
+
+    public function pluginSettingsPageContent() {
+        ?>
+        <div class="wrap">
+            <h2><?php echo BfcConstants::SETTINGS_PAGE_TITLE ?></h2>
+            <form method="POST" action="options.php">
+                <?php
+                settings_fields(BfcConstants::SETTINGS_PAGE_SLUG);
+                do_settings_sections(BfcConstants::SETTINGS_PAGE_SLUG);
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    public function btcpaySectionCallback(){
+        echo BfcConstants::SETTINGS_PAGE_BTCPAY_SECTION_DESCRIPTION;
+    }
+
+    public function setupFields() {
+        add_settings_field(BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK, BfcConstants::SETTINGS_PAGE_BTCPAY_APP_LINK_FIELD_DESCRIPTION, array($this, 'btcpayAppLinkFieldCallback'), BfcConstants::SETTINGS_PAGE_SLUG, self::BTCPAY_SECTION_KEY);
+    }
+
+    public function btcpayAppLinkFieldCallback($arguments){
+        echo '<input name="' . BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK . '" id="' . BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK . '" type="text" value="' . get_option(BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK) . '" />';
+    }
+
+    public function getBtcPayAppURL() {
+        return get_option(BfcConstants::DB_STORAGE_KEY_BTCPAY_APP_LINK);
+    }
+}
